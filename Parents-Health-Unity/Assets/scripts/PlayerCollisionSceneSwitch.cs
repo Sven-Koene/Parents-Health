70a0ff8aa61ceedb76c0d4f054dc0ccbd9537954
Sven-Koene <32331946+Sven-Koene@users.noreply.github.com>
2020-07-01 12:14:33 +0200

    the project

---
+++ Parents-Health-Unity/Assets/scripts/PlayerCollisionSceneSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerCollisionSceneSwitch : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(4);
+        }
+    }
+}
